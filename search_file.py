2a1c9e3dadd6cfa453e097122e73b171ab126311
myanaajay77 <myana.ajay77@gmail.com>
2021-03-29 21:39:18 +0530

    jenkinstest

---
+++ search_file.py
@@ -0,0 +1,7 @@
+import os
+for root,dirnames,files in os.walk("D:"):
+    """for file in files:
+        if file.endswith(".pdf"):
+            print(root)"""
+    for dirctoryname in dirnames:
+        if dirctoryname.
